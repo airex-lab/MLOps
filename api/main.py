ff103483bc4e47d0d30e5ce111573cf2d1812bba
Lokesh Mohanty <lokesh1197@gmail.com>
2023-07-12 15:58:27 +0530

    update Dockerfile

--- api/main.py
+++ api/main.py
@@ -1,10 +1,27 @@
 from fastapi import FastAPI
+from datetime import datetime
 
 app = FastAPI()
 
+@app.post("/predict")
+def predict(file: UploadFile = File(...)):
+    try:
+        contents = file.file.read()
+        with open(file.filename), 'wb') as f:
+            f.write(contents)
+    except Exception as e:
+        print(e)
+        return { 'error': 'Error during file upload' }
+    finally:
+        file.file.close()
+
+    return { 'msg': f'Successfully uploaded {file.filename}' }
+
 @app.get('/time')
 async def getTime():
-    return { 'time': '2021-09-01 12:00:00' }
+    now = datetime.now()
+    time = now.strftime("%Y-%m-%d %H:%M:%S")
+    return { 'time': time }
 
 @app.get('/')
 async def ping():
